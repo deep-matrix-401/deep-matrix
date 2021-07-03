9f7df26011ffe321368d7ec52387da6067b76241
amarh-ayman <amarhbishtawi@gmail.com>
2021-07-03 15:30:40 +0300

    Add the camera code

--- camera codes/camera_code.py
+++ camera codes/camera_code.py
@@ -1,8 +1,5 @@
 import cv2 
 import uuid
-### livelong - on fornt of ur face and left side using the other hand.
-### perfect - on fornt of ur face and left side using the other hand.
-### stop - on fornt of ur face and left side using the other hand.
 labels=['livelong','perfect','stop','yes','no','thumbsup']
 label='livelong'
 key = cv2. waitKey(1)
